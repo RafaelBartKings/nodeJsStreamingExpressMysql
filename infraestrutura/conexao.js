34cfdb5bf6891492df376bba854a6cc2e63c1b82
rafaelreis <rafyangtj@gmail.com>
2021-05-07 19:23:22 -0300

    update

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -4,7 +4,7 @@ const conexao = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'R26Etlm11!20',
+    password: 'Rxxxxxxxxxx',
     database: 'agenda-petshop'
 });
 
